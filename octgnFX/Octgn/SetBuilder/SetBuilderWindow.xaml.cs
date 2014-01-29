df2d0dcdd8242a65f41c33ca607050f6e578332e
DarkSir23 <jason@fortsackville.com>
2014-01-28 22:48:10 -0500

    SetBuilder Stuff

--- octgnFX/Octgn/SetBuilder/SetBuilderWindow.xaml.cs
+++ octgnFX/Octgn/SetBuilder/SetBuilderWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -16,11 +17,31 @@ namespace Octgn.SetBuilder
     /// <summary>
     /// Interaction logic for SetBuilderWindow.xaml
     /// </summary>
-    public partial class SetBuilderWindow : Window
+    public partial class SetBuilderWindow : INotifyPropertyChanged
     {
         public SetBuilderWindow()
         {
             InitializeComponent();
         }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            WindowManager.SetBuilder = null;
+        }
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
     }
+
+
+
 }
